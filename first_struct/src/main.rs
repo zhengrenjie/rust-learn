2a1bdeca90d35cea496f4275135bb0945cdc6f85
zhengrenjie <1354334940@qq.com>
2019-12-27 18:19:59 +0800

    fix

--- first_struct/src/main.rs
+++ first_struct/src/main.rs
@@ -7,6 +7,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn main() {
@@ -15,5 +19,17 @@ fn main() {
         height: 32
     };
 
+    let rec_small = Rectangle {
+        width: 32,
+        height: 16,
+    };
+
+    let rec_bigger = Rectangle {
+        width: 64,
+        height: 16,
+    };
+
     println!("{}", rec.area());
+    println!("{}", rec.can_hold(&rec_small));
+    println!("{}", rec.can_hold(&rec_bigger));
 }
